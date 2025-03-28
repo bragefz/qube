6a208821764eab3cf51bbaee61ff2b404b00182b
bragefz <bvihle@stud.ntnu.no>
2025-03-28 14:17:25 +0100

    Reformatted launch file and added params.
    Changed the structure of the launch to make it more organized, now returning a list of the args and nodes instead of defining them in the return statement. Added parameters baud_rate, device, and simulation.

--- qube_bringup/launch/bringup.launch'.py
+++ qube_bringup/launch/bringup.launch'.py
@@ -10,6 +10,22 @@ import xacro
 
 
 def generate_launch_description():
+    # Declare launch arguments with default values
+    baud_rate_arg = DeclareLaunchArgument(
+        'baud_rate', default_value='115200',
+        description='Communication frequency'
+    )
+
+    device_arg = DeclareLaunchArgument(
+        'device', default_value='/dev/ttyUSB0',
+        description='USB device'
+    )
+
+    simulation_arg = DeclareLaunchArgument(
+        'simulation', default_value='False',
+        description='Set whether the program should control real (False) or simulated hardware (True)'
+    )
+
     qube_driver_launch = os.path.join(
         get_package_share_directory('qube_driver'),
         'launch',
@@ -21,24 +37,48 @@ def generate_launch_description():
         "urdf",
         "controlled_qube.urdf.xacro"
     )
-    robot_description_content = xacro.process_file(xacro_file).toxml()
+
+    # Process the xacro file with parameters
+    mappings = {
+        'baud_rate': LaunchConfiguration('baud_rate'),
+        'device': LaunchConfiguration('device'),
+        'simulation': LaunchConfiguration('simulation')
+    }
+    robot_description_content = xacro.process_file(xacro_file, mappings=mappings).toxml()
+
+    # Define the qube_driver launch include
+    qube_driver_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(qube_driver_launch),
+        launch_arguments=mappings.items()
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        # arguments=['-d', os.path.join(get_package_share_directory("joint_description"), 'config', 'your_config.rviz')]
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_content}]
+    )
+
+    args = [
+        baud_rate_arg,
+        device_arg,
+        simulation_arg
+    ]
+
+    nodes_to_start = [
+        qube_driver_include,
+        rviz_node,
+        robot_state_publisher_node
+    ]
+
 
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(qube_driver_launch)
-        ),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            # add the config file which is to be created first time rviz is launched
-            # arguments=['-d', os.path.join(get_package_share_directory("joint_description"), 'config', '<navn på fil>.rviz')]
-        ),
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': robot_description_content}]
-        )
+        *args,
+        *nodes_to_start
     ])
