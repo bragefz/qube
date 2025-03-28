71a6b37806159bae86b75185200a278546be1ef0
bragefz <bvihle@stud.ntnu.no>
2025-03-28 15:53:49 +0100

    Fixed package.xml and setup.py files
    Needs to be double checked, might have added unnecessary dependencies.

--- qube_controller/setup.py
+++ qube_controller/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'qube_controller'
 
@@ -10,11 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='zappa',
-    maintainer_email='bvihle@stud.ntnu.no',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
--- qube_description/setup.py
+++ qube_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'qube_description'
 
@@ -10,11 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.xacro'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='zappa',
-    maintainer_email='bvihle@stud.ntnu.no',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
