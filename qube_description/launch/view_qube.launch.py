2dc4d80b5bee5b46f736d940d5ecc2ceec3bf038
Eirik <eirikror@ntnu.no>
2025-04-22 22:09:57 +0200

    Forbedret dokumentasjon i URDF og launch fil

--- qube_description/launch/view_qube.launch.py
+++ qube_description/launch/view_qube.launch.py
@@ -7,16 +7,16 @@ import os
 
 
 def generate_launch_description():
-    # Get path to the URDF file
+    # finner path for qube.urdf.xacro filen
     urdf_file = os.path.join(get_package_share_directory('qube_description'), 'urdf', 'qube.urdf.xacro')
 
-    # Process URDF file using Command substitution
+    #lager robot_desciption_content som konverterer xacro til urdf
     robot_description_content = Command(['xacro ', urdf_file])
 
-
+    #robot_description parameter som inneholder den konverterte urdf modellen
     robot_description = {'robot_description': ParameterValue(robot_description_content, value_type=str)}
 
-
+    #Lager robot_state_publisher_node som er noden som skal publisere tilstandene til robotens ledd
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -33,15 +33,17 @@ def generate_launch_description():
         output='screen'
     )
 
+    #Lager en statisk transform publisher som definerer forholdet mellom "world" og "map" koordinatsystemene
     static_tf_node = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='static_transform_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'world', 'map']
+        arguments=['0', '0', '0', '0', '0', '0', 'world', 'map'] #Argumentene ['0', '0', '0', '0', '0', '0'] gir posisjon (x,y,z) og (r,p,y)
     )
 
-
+    #prøver å finne en RViz config fil
     try:
+        #finner stien til config filen og setter opp RViz med denne configen
         rviz_config = os.path.join(get_package_share_directory('qube_bringup'), 'config', 'qube_rviz_config.rviz')
         rviz_node = Node(
             package='rviz2',
@@ -51,7 +53,7 @@ def generate_launch_description():
             output='screen'
         )
     except:
-
+        #starter RViz standard hvis den ikke finner config fil eller om den ikke finnes
         rviz_node = Node(
             package='rviz2',
             executable='rviz2',
@@ -59,7 +61,7 @@ def generate_launch_description():
             output='screen'
         )
 
-
+    #returnerer en "LaunchDescription" med rekkefølgen noden skal startes på
     return LaunchDescription([
         static_tf_node,
         joint_state_publisher_gui_node,
